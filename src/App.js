a1ef9c2803ecb27aeac54e45728e4de780406f3f
snatu1996 <shubham.natu@cognizant.com>
2020-12-03 20:48:51 +0530

    Global state management using useReducer

--- src/App.js
+++ src/App.js
@@ -1,8 +1,26 @@
 
-import {useContext} from 'react'
+import {useContext,useReducer} from 'react'
 import {UserInfo} from './index'
 
+const initialState = {
+    count: 0
+}
+
+const reducer = (state,action) => {
+    switch(action.type) {
+        case "increment" : 
+        return {count: state.count + 1 }
+        case "decrement" :
+            return {count: state.count - 1}
+        case "reset" :
+            return initialState
+        default : 
+        return initialState
+    }
+}
+
 export default function App () {
+    const [state,dispatch] = useReducer(reducer,initialState)  //useReducer provides with updated state and dispatch function, used to dispatch action
     const value = useContext(UserInfo)
 
     return (
@@ -12,7 +30,16 @@ export default function App () {
         // </UserInfo.Consumer> // Using consumer we need to pass in a render props function
 
         //With hooks
+        <>
 
         <div>Hello,  {value}</div>
+
+        <div>
+            count: {state.count}
+            <button onClick={() => dispatch({ type: "increment"})}>Increment!!!</button>
+            <button onClick={() => dispatch({type: "decrement"})}>Decrement!!!</button>
+            <button onClick={() => dispatch({type: "reset"})}>Reset!!!</button>
+        </div>
+        </>
     )
 }
